16923ec7bb82fde78f7b0b7acffdde155274cf58
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-06-30 13:29:08 +0530

    connect  backend to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app = express()
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    
+  });const app = express()
 
 app.listen(3000, ()=>{
     console.log("runiun");
     
 })
+
+//
