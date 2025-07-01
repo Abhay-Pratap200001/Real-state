7f54f688e3f72c883bd407cf1b94e30924a5fa43
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-07-01 11:34:39 +0530

    Created a midleware  error handler for error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 
 
-export const signup = async(req,res) =>{//Creating user signup controller
+export const signup = async(req,res,next) =>{//Creating user signup controller
 const {username, email, password} = req.body;
 const hashedPassword = bcryptjs.hashSync(password, 10);// encrypting the password
 const newUser = new User({username, email, password: hashedPassword});
 try {
-    await newUser.save(); //saving the new user in database
-    res.status(201).json('user Created check you database')  
+       await newUser.save(); //saving the new user in database
+        res.status(201).json('user Created check you database')  
 } catch (error) {
-   res.status(500).json("Dublicate user" + error.message) 
-}  
-}
+  next(error) // using error handler from index.js  
+}}
