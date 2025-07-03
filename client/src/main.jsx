423b41f80e02baea45e1826a192579a8cfafe814
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-07-03 10:21:00 +0530

    Add redux-presist to store data on localstorage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react' // step 2 cover app.jsx to provider
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store.js'
-import {Provider} from 'react-redux'
+
+import {persistor, store } from './redux/store.js'// Import the Redux store and persistor from the store file
+
+import {Provider} from 'react-redux'// Import Provider to make Redux store available to the app
+
+import { PersistGate } from 'redux-persist/integration/react'// Import PersistGate to delay rendering until persisted state is loaded
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}> 
+  <PersistGate loading={null} persistor={persistor}>{/* PersistGate ensures that Redux Persist loads saved state before showing the app */}
     <App />
+    </PersistGate>
   </Provider>,// wraps your entire app and gives access to the Redux store.
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'  //step 1 make store 
-import  userReducer  from '../user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'  //step 1 make store 
+import  userReducer  from '../user/userSlice';// Import user reducer (authentication-related state
+import {persistReducer,persistStore} from "redux-persist";// For saving Redux state in localStorage
+import storage from "redux-persist/lib/storage";// Use localStorage as default storage
+
+
+const rootReducer = combineReducers({user: userReducer});// Step 2: Combine all reducers (we have only one now: user)
+
+const persistConfig = {// Step 3: Configuration for Redux Persis
+  key: 'root',
+  storage,
+  version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)// Step 4: Create a persisted version of the root reducer
 
 export const store = configureStore({ //Creates the global Redux store.
-  reducer: {user: userReducer}, // Manages authentication-related state (loading, user info, error).
+  reducer : persistedReducer,  // Use the persisted reducer here
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false, //Allows non-serializable values like cookies if used in actions.
   }),
 })
+
+export const persistor = persistStore(store)
