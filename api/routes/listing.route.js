c7232d0abd2771ae496e2191d4905045c99606c8
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-07-13 20:44:03 +0530

    Creted api for search listing

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing,  deleteListing, updateListing,getListing} from '../controllers/listing.controller.js';
+import { createListing,  deleteListing, updateListing, getListing, getListings} from '../controllers/listing.controller.js';
  export const route = express.Router();
 
-route.post("/create", verifyToken, createListing)
-route.delete("/delete/:id", verifyToken, deleteListing)
-route.get("/get/:id",  getListing)
-route.post("/update/:id", verifyToken, updateListing)
+route.post("/create", verifyToken, createListing)//crete listing route
+route.delete("/delete/:id", verifyToken, deleteListing)// delete listing route
+route.post("/update/:id", verifyToken, updateListing)// update listing route only authorized user valid
+route.get("/get/:id",  getListing)// get single listing according to id
+route.post("/get", getListings)
+
 
 
 
