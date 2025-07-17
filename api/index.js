2ed4ff6af1d6b44ce8784fff8a817b43d8e43434
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-07-17 12:09:22 +0530

    Add pre indtallation and add dir for deploying

--- api/index.js
+++ api/index.js
@@ -5,31 +5,39 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
-
+import path from 'path';
 dotenv.config();
+
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-
-//connecting server to database
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(3000, () => {
-      console.log("Server running on port 3000");
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    
-  });
-
-
-
 app.use('/api/user', userRouter);//update/delete
 app.use('/api/auth' , authRouter)//signup/api/singin/api
 app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
+
+//connecting server to database
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+  console.log("MongoDB connected");
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+})
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
+  
+});
+
 
 //error middleware 
 app.use((err, req, res, next) => {
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
 import { createListing,  deleteListing, updateListing, getListing, getListings} from '../controllers/listing.controller.js';
- export const route = express.Router();
 
-route.post("/create", verifyToken, createListing)//crete listing route
-route.delete("/delete/:id", verifyToken, deleteListing)// delete listing route
-route.post("/update/:id", verifyToken, updateListing)// update listing route only authorized user valid
-route.get("/get/:id",  getListing)// get single listing according to id
-route.get("/get", getListings)
+const router = express.Router();
 
-export default route
+router.post("/create", verifyToken, createListing)//crete listing route
+router.delete("/delete/:id", verifyToken, deleteListing)// delete listing route
+router.post("/update/:id", verifyToken, updateListing)// update listing route only authorized user valid
+router.get("/get/:id",  getListing)// get single listing according to id
+router.get("/get", getListings)
+
+export default router
